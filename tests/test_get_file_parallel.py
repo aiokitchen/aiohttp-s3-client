dbd7a364e2db31451a918abbee3d986f24b313c4
Yuri Shikanov <dizballanze@users.noreply.github.com>
2021-08-31 18:44:09 +0300

    Feature/parallel get object (#4)
    * Parallel object download
    * Fix types
    * Fix imports
    * Add tests
    * Add docs
    * Remove unused local variables
    * Fix docs
    * Improve download files fallback when pwrite is not available
    * Test that file was cleaned up on downloading fail

---
+++ tests/test_get_file_parallel.py
@@ -0,0 +1,76 @@
+import asyncio
+import os
+import secrets
+
+import pytest
+
+from aiohttp_s3_client import S3Client
+from aiohttp_s3_client.client import AwsDownloadError
+
+
+async def test_get_file_parallel(s3_client: S3Client, tmpdir):
+    data = b"Hello world! " * 100
+    object_name = "foo/bar.txt"
+    await s3_client.put(object_name, data)
+    await s3_client.get_file_parallel(
+        object_name,
+        tmpdir / "bar.txt",
+        workers_count=4,
+    )
+    assert (tmpdir / "bar.txt").read_binary() == data
+
+
+async def test_get_file_parallel_without_pwrite(
+    s3_client: S3Client, tmpdir, monkeypatch,
+):
+    monkeypatch.delattr("os.pwrite")
+    data = b"Hello world! " * 100
+    object_name = "foo/bar.txt"
+    await s3_client.put(object_name, data)
+    await s3_client.get_file_parallel(
+        object_name,
+        tmpdir / "bar.txt",
+        workers_count=4,
+    )
+    assert (tmpdir / "bar.txt").read_binary() == data
+
+
+async def test_get_file_that_changed_in_process_error(
+    s3_client: S3Client, tmpdir,
+):
+    object_name = "test/test"
+
+    def iterable():
+        for _ in range(8):  # type: int
+            yield secrets.token_hex(1024).encode()
+
+    await s3_client.put_multipart(
+        object_name,
+        iterable(),
+        workers_count=4,
+    )
+
+    async def upload():
+        await asyncio.sleep(0.05)
+        await s3_client.put_multipart(
+            object_name,
+            iterable(),
+            workers_count=4,
+        )
+
+    with pytest.raises(Exception) as err:
+        await asyncio.gather(
+            s3_client.get_file_parallel(
+                object_name,
+                tmpdir / "temp.dat",
+                workers_count=4,
+                range_step=128,
+            ),
+            upload(),
+        )
+
+    assert err.type is AwsDownloadError
+    assert err.value.args[0].startswith(
+        "Got wrong status code 412 on range download of test/test",
+    )
+    assert not os.path.exists(tmpdir / "temp.dat")
